5a71dc0694d91196006dc185ef29289592937388
Khanh Bui <duykhanh.eric@gmail.com>
2026-02-22 08:16:58 -0700

    file sharing

--- build/CMakeFiles/Termicomm.dir/src/main.cpp.o.d
+++ build/CMakeFiles/Termicomm.dir/src/main.cpp.o.d
@@ -308,4 +308,5 @@ CMakeFiles/Termicomm.dir/src/main.cpp.o: \
  /usr/include/c++/13/cstring /usr/include/string.h /usr/include/strings.h \
  /usr/include/c++/13/numeric /usr/include/c++/13/bits/stl_numeric.h \
  /usr/include/c++/13/pstl/glue_numeric_defs.h /usr/include/c++/13/any \
+ /mnt/c/Users/duykh/Documents/homework/Termicomm/src/../include/base64.hpp \
  /usr/include/portaudio.h
